8131fa9ceb7762ce43c9e57b0069f71a06d00cca
JD <noideal@users.noreply.github.com>
2019-11-16 20:43:19 -0700

    suricata fix and test changes

--- stuff/suricata.sh
+++ stuff/suricata.sh
@@ -1,26 +1,42 @@
 #!/bin/sh
 
 # Install Suricata.
-# $ sudo apt-get install software-properties-common
+# $ sudo apt install software-properties-common
 # $ sudo add-apt-repository ppa:oisf/suricata-stable
-# $ sudo apt-get update
-# $ sudo apt-get install suricata
+# $ sudo apt update
+# $ sudo apt install suricata
+# 
+# !! compile script with 'libcap_ng' for drop permission
 #
 # Setup Suricata configuration.
 #
 # In /etc/default/suricata, set RUN to "no".
 #
-# In /etc/suricata/suricata.yaml apply the following changes;
+# In /etc/suricata/suricata-cuckoo.yaml apply the following changes;
 # * Set "unix-command.enabled" to "yes".
 # * Set "unix-command.filename" to "cuckoo.socket".
 # * Set "outputs.eve-log.enabled" to "yes".
 # * Set "run-as.user to "your cuckoo user"
 # * Set "run-as.group to "your cuckoo user group"
+# * Set "default-rule-path" to "/etc/suricata/rules/"
 # * TODO More items.
+# $ sudo suricata-update -c /etc/suricata/suricata-cuckoo.yaml -o /etc/suricata/rules
 #
 # Add "@reboot /opt/cuckoo/utils/suricata.sh" to the root crontab.
-# This will reload suricata rules
-# Add "15 * * * * /usr/bin/suricatasc -c reload-rules" to the root crontab.
+#
+# Quick script to update the suricata conf (need to test)
+# # !/usr/bin/env python
+# # import yaml
+# #
+# # with open('/etc/suricata/suricata-cuckoo.yaml') as f:
+# #     data = yaml.load(f, Loader=yaml.FullLoader)
+# #     data['unix-command']['enabled'] = 'yes'
+# #     data['unix-command']['filename'] = 'cuckoo-socket'
+# #     data['outputs']['eve-log']['enabled'] = 'yes'
+# #     data['run-as']['user'] = 'cuckoo'
+# #     data['run-as']['group'] = 'cuckoo'
+# #     data['default-rule-path'] = '/etc/suricata/rules/'
+# #     f.write(yaml.dump(data, default_flow_style=False))
 
 . /etc/default/cuckoo
 
@@ -29,11 +45,13 @@ if [ "$SURICATA" -eq 0 ]; then
     exit
 fi
 
-mkdir /var/run/suricata
-chown cuckoo:cuckoo /var/run/suricata
+# Shouldn't need this anymore with change to the socket/rules/log paths and drop permission
+mkdir -p /var/run/suricata
+chown -R root:cuckoo /var/run/suricata
+chmod -R 775 /var/run/suricata
 
-suricata --unix-socket -k none -D
+suricata -c /etc/suricata/suricata-cuckoo.yaml --unix-socket=/opt/cuckoo/cuckoo.socket -k none -l /var/tmp -D
 
-while [ ! -e /var/run/suricata/cuckoo.socket ]; do
+while [ ! -e /opt/cuckoo/cuckoo.socket ]; do
     sleep 1
 done
